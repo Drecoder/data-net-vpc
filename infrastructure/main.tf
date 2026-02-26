abdb7f05c32cbe39a6cb8ccdce427a9d11cfd39f
Andres Arias <ariasa83@gmail.com>
2026-02-25 20:46:23 -0600

    test: add insecure bucket to verify Checkov security gate

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -33,3 +33,13 @@ module "compute" {
   min_instances = 0
   max_instances = 3
 }
+
+# This resource is for testing Checkov security gates
+resource "google_storage_bucket" "insecure_test_bucket" {
+  name          = "${var.project_id}-insecure-test"
+  location      = "US"
+  force_destroy = true
+
+  # CHECKOV TRIGGER: Missing 'public_access_prevention' 
+  # CHECKOV TRIGGER: Missing 'uniform_bucket_level_access'
+}
